4bd5289740c476b6304af65040e141e268f806bc
DuyLe1605 <leminhduy160505@gmail.com>
2025-07-03 14:05:44 +0700

    Kết nối Mongo Atlas bằng MongoDB Driver

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express'
-import userRouter from '~/routes/users.routes'
-const app = express()
+import userRouter from '~/routes/users.route'
+import databaseService from '~/services/database.service'
 
+const app = express()
 const port = 4000
+
 app.use(express.json())
 app.use('/user', userRouter)
+
+databaseService.connect().catch(console.dir)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ src/services/database.service.ts
@@ -0,0 +1,32 @@
+import { MongoClient, ServerApiVersion } from 'mongodb'
+import 'dotenv/config'
+
+const uri = process.env.DB_URI as string
+
+console.log(process.env.DB_URI)
+
+class DatabaseService {
+  private client: MongoClient
+  constructor() {
+    this.client = new MongoClient(uri, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true
+      }
+    })
+  }
+
+  async connect() {
+    try {
+      // Send a ping to confirm a successful connection
+      await this.client.db('admin').command({ ping: 1 })
+      console.log('Pinged your deployment. You successfully connected to MongoDB!')
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await this.client.close()
+    }
+  }
+}
+
+export default new DatabaseService()
